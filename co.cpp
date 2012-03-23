c1d17ccdfd1fd0587d8babc1025ae08ba88eb48e
shrimai <shrimai@68905dcb-69ff-49f0-8bea-f10a1924dab7>
2012-03-23 10:47:43 +0000

    Contains code to read input from a database. Very basic.
    git-svn-id: file:///home/siddhartha/graffiti/trunk@17 68905dcb-69ff-49f0-8bea-f10a1924dab7

---
+++ co.cpp
@@ -0,0 +1,33 @@
+void input_points()
+{
+	for(y=0;y<49;y++)
+	{
+		for(c=0;c<244;c++)
+		{
+			scanf("%f",&a[c][y]);
+			if(c==0){
+				ymin=a[c][y];
+				total[y]=a[c][y];
+			}
+			else{
+				if(a[c][y]<ymin)
+				ymin=a[c][y];
+				total[y]+=a[c][y];
+			}
+			if(a[c][y]>ymax)
+			ymax=a[c][y];
+			
+		}
+		mean[y]=total[y]/244;
+	}
+	float width=(float)ww/(float)unit;
+	float index=-width;
+	for(int y=0;y<49;y++)
+	{
+		for(int c=0;c<244;c++)
+		{
+			p[c][y].setCoords(index, (a[c][y]*3.0f)/mean[y],0.0f);
+			index+=width/244;	
+		}
+	}	
+}
