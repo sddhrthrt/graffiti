f02db573d16d1b025f9a0bafa1db6b842627b27c
sddhrthrt <sddhrthrt@gmail.com>
2012-05-07 15:09:50 +0530

    Deleted

--- trunk/Mainpage.h
+++
@@ -1,9 +0,0 @@
-/**\mainpage  Graffiti
-*\authors Akhil, Anantha, Chandrakanth, Shrimai, Siddhartha
-*@section Introduction 
-*This is the manual for our project, Graffiti, OpenGL Implementation of Scientific Data Visualization. <br>
-*The project might seem simple, but mind you, we have used only GlVertex2f() command to draw *everything* here!
-*@section Download
-*Feel free to visit out project at <a href="http://github.com/sddhrthrt/graffiti">github.com/sddhrthrt/graffiti</a>
-*
-*/
